727ee294dab1058592eb1dece414e5d6a1f0de40
Aryan Malik <Aryanmalik19.am@gmail.com>
2021-08-18 16:09:07 +0530

    logo added

--- infinity/src/App.js
+++ infinity/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css'
+import { AuthProvider } from './context/AuthContext';
+import routes from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import PATHS from './config/path';
+import { PrivateRouteDashboard, PrivateRouteLogin } from './routes/PrivateRoute';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <PrivateRouteLogin exact path={PATHS.DASHBOARD} component={Dashboard} />
+          <PrivateRouteDashboard exact path={PATHS.HOME} component={Home} />
+          {
+            routes.map((route, idx) => {
+              return (
+                <Route key={idx} {...route} />
+              )
+            })
+          }
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
+  )
 }
 
 export default App;
